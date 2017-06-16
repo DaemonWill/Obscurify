5f5528deb30284b0a79a99d0a73d6687dbe97c8c
Daimen Williams <daimenwill@gmail.com>
2017-06-16 03:39:30 -0400

    in the middle of restructuring

--- obscurifyModule.js
+++
@@ -1,9 +0,0 @@
-define(['config',
-  'homeController'],
-
-  function(config, homeController){
-    var app = angular.module('obscurifyMusic', ['ngRoute']);
-    app.config(config);
-    app.controller('homeController', homeController);
-
-});
